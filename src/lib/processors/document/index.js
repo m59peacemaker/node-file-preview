c98b03aba864027c580fd4c9d11ebe05ba55ff5b
Johnny Hauser <johnnyhauser@gmail.com>
2020-06-23 09:37:12 -0500

    removed unused libreoffice macro stuff for improving spreadsheet formatting - that should not be a concern of this project and can be accomplished independently before passing the file to filePreview

--- src/lib/processors/document/index.js
+++ src/lib/processors/document/index.js
@@ -1,8 +1,17 @@
-const convertDocumentToPreviewReadyPdf = require('./convertDocumentToPreviewReadyPdf')
+const path = require('path')
+const replaceExtension = require('replace-ext')
+const convertDocument = require('./unoconv/convertDocument')
 const supportedTypes = require('./supportedTypes')
 
 module.exports = {
-	process: ({ document, ...options }) =>
-		convertDocumentToPreviewReadyPdf({ ...document, ...options }),
+	process: ({ document, inputFilePath, outputDirectoryPath }) =>
+		convertDocument({
+			...document,
+			inputFilePath,
+			outputFilePath: path.join(
+				outputDirectoryPath,
+				replaceExtension(path.basename(inputFilePath), '.pdf')
+			)
+		}),
 	supportedTypes
 }
