c6c718c20db47ca7c036ef9ca639ef1e48cc676d
Johnny Hauser <johnnyhauser@gmail.com>
2020-06-23 08:44:46 -0500

    it seems that trying to convert documents while unoconv has not fully started its libreoffice instance causes crashes. Switched to a solid strategy of watching the libreoffice port to determine when the listener is up

--- src/lib/processors/document/unoconv/listener.js
+++ src/lib/processors/document/unoconv/listener.js
@@ -1,15 +1,18 @@
 const { spawn } = require('child_process')
+const tryConnect = require('try-net-connect')
 const terminate = require('util').promisify(require('terminate'))
 
 const defaults = {
 	port: 2002,
-	silent: false
+	silent: false,
+	initTimeoutMs: 3000
 }
 
 const listen = (options = defaults) => {
-	const { port, silent } = { ...defaults, ...options }
+	const { port, silent, initTimeoutMs } = { ...defaults, ...options }
 	// It is very difficult to get the logs from the unoconv process. Using `script --command` is the best way I could figure out.
 	const proc = spawn('script', [ '--quiet', '--command', `unoconv -vvv --listener --port ${port}`, '/dev/null' ])
+	const terminateProc = async () => terminate(proc.pid).catch(() => {})
 
 	if (!silent) {
 		proc.stdout.pipe(process.stdout)
@@ -19,14 +22,16 @@ const listen = (options = defaults) => {
 	return {
 		init: new Promise((resolve, reject) => {
 			proc.on('error', reject)
-			proc.stdout.on('data', data => {
-				if (data.toString().includes('Start listener on')) {
-					resolve()
-				}
-			})
+			tryConnect({
+				retry: 500,
+				retries: 10,
+				host: '127.0.0.1',
+				port: port
+			}).on('connected', resolve)
+				.on('timeout', reject)
 		}),
 		process: proc,
-		terminate: async () => terminate(proc.pid).catch(() => {})
+		terminate: terminateProc
 	}
 }
 
